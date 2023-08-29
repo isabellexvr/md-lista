6895cc30b81c85b69c5779f1d3089251d0dca031
TheMarksan <marcosmelodossantos.com@gmail.com>
2023-08-28 21:14:34 -0300

    Questão 3

--- Q3/q3.c
+++ Q3/q3.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
-    int bool, f, n;
-
-    scanf("%d", &n);
-
-    bool = 1;
+int isPrime(int n){
+    int bool = 1;
 
     if(n < 2){
         bool = 0;
     }
-
     for (int i = 3; i * i <= n; i += 2) {
         if(n % i == 0){
             bool = 0;
@@ -20,11 +14,25 @@ int main()
         }
     }
 
-    if(bool){
-        printf("É primo\n");
-    }else{
-        printf("Não é primo\n");
+    return bool;
+}
+
+int main(){
+    int i, number;
+    scanf("%d", &number);
+
+    for(i=number;i>=2;i--){
+        if(i%2==0){
+            continue;
+        }else{
+            if(isPrime(i)){
+                if(number%i==0){
+                    printf("%d\n", i);
+                }
+            }
+        }
     }
+
     
     return 0;
 }
