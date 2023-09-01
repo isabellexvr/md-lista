9b75a1f5c32e362d2d83e3e7532384d0791a2db8
Marcos Melo <124604286+TheMarksan@users.noreply.github.com>
2023-09-01 16:18:18 -0300

    Questões do teorema xing ling

--- Q10/q10.c
+++ Q10/q10.c
@@ -1,29 +1,86 @@
-#include <stdio.h>
-#include <math.h>
+#include <stdio.h> 
 
-int main()
-{
-    int bool, f, n;
-
-    scanf("%d", &n);
-
-    bool = 1;
-
-    if(n < 2){
-        bool = 0;
+int extendMDC(int n1, int n2, int resto, int *addressA, int *addressB){
+    int a=1, b=0, quociente, candidateS, s, mdc;
+    
+    *addressA = n1;
+    *addressB = n2;
+    
+    while( (n2!=0) && (n1 % n2 != 0) ){
+        resto = n1 % n2;
+        quociente = n1 / n2;
+        candidateS = a - (b * quociente);
+        a = b;
+        b = candidateS;
+        n1 = n2; 
+        n2 = resto; 
     }
+    
+    if(candidateS < 0){
+       candidateS += *addressB;
+    }
+    
+    return candidateS;
+}
 
-    for (int i = 3; i * i <= n; i += 2) {
-        if(n % i == 0){
-            bool = 0;
+int euclides(int A, int D){
+
+    if(A % D == 0){
+        return D;
+    }else{
+        return euclides(D, (A % D));
+    }
+}
+
+int main() { 
+    int n, mdcCandidato, answer = 0, flag=1;
+    
+    printf("Quantas congruências lineares serão? ");
+    scanf("%d", &n);
+    
+    int divisores[n], restos[n], M = 1, Mn[n], sArr[n];
+    
+    
+    for( int i = 0 ; i < n ; i++ ){
+        printf("Valores de b e m:\n");
+        scanf("%d %d", &restos[i], &divisores[i]);
+    }
+    
+    for( int i = 0 ; i < n ; i++ ){
+        if(i==n-1){
             break;
+        }else if(euclides(divisores[i], divisores[i+1])!=1){
+            flag = 0;
         }
     }
-
-    if(bool){
-        printf("É primo\n");
+    
+    if(flag==0){
+        printf("Nao ha solucoes para esse sistema.");
+        return 0;
     }else{
-        printf("Não é primo\n");
+        
+        for(int i = 0 ; i < n ; i++){
+            M *= divisores[i];
+        }
+        
+        for( int i = 0 ; i < n ; i++ ){
+            Mn[i] = (M/divisores[i]);
+            int a,b;
+            sArr[i] = extendMDC( (M/divisores[i]), divisores[i], 0, &a, &b);
+        }
+        
+        for( int i = 0 ; i < n ; i++ ){
+             answer += (Mn[i]*restos[i]*sArr[i]);
+        }
+        
+        answer = answer % M;
+        
+        printf("Essas sao as %d congruencias:\n", n);
+        for(int i=0;i<n;i++){
+            printf("Congruencia %d: X ≡ %d mod %d\n", i+1, restos[i], divisores[i]);
+        }
+        
+        printf("O valor de X eh: %d\n", answer);
     }
     
     return 0;
