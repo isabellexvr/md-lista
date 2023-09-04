c5f624ff91edd8d22bdf9282a2333bf08bac6978
isabellexvr <isabellexavier59@gmail.com>
2023-09-04 01:23:09 -0300

    fix: q8 s value bigger than M

--- Q8/q8.c
+++ Q8/q8.c
@@ -20,6 +20,12 @@ int extendMDC(int n1, int n2, int resto, int *addressA, int *addressB){
     mdc = n2;
     
     t = (mdc - (*addressA * s) ) / *addressB;
+
+    if(s < 0){
+        s += *addressB;
+    }else if(s >= *addressB){
+        s = s % *addressB;
+    }
     
     return s;
 }
@@ -39,8 +45,14 @@ int congruencia(int A, int B, int M, int numberOfSolutions){
     int solutions, s, addressA = A, addressB = B, x, solution;
     
     s = extendMDC(A,M,0, &addressA, &addressB);
+    printf("A: %d, M: %d\n", A, M);
+    printf("s: %d\n", s);
     
     x = B * s;
+
+    if(x > M){
+        x = x % M;
+    }
     
     if(numberOfSolutions == 1){
         solution = (x + M*0);
